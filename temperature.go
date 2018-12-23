f8e9fc077ad4be6d5f00a917c8c43a2657f636d0
valnmv <vnaumov@yahoo.com>
2018-12-23 06:47:12 +0200

    Add comments

--- temperature.go
+++ temperature.go
@@ -1,9 +1,11 @@
 package temperature
 
+// CtoF converts temperature from Celsius to Fahrenheit
 func CtoF(c float64) float64 {
 	return (c * (9 / 5)) + 32
 }
 
+// FtoC converts temperature from Fahrenheit to Celsius
 func FtoC(f float64) float64 {
 	return (f - 32) * 5 / 9
 }

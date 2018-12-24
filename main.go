6ab12a45e85f7015b284e63f6172c44eed1ad45c
valnmv <vnaumov@yahoo.com>
2018-12-24 06:39:20 +0200

    Simplest web server

--- main.go
+++
@@ -1,7 +0,0 @@
-package main
-
-import "log"
-
-func main() {
-	log.Printf("This is a log message")
-}
---
+++ web-server/main.go
@@ -0,0 +1,12 @@
+package main
+
+import "net/http"
+
+func hello(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Hello World\n"))
+}
+
+func main() {
+	http.HandleFunc("/", hello)
+	http.ListenAndServe(":8000", nil)
+}

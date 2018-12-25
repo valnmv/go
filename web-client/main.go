8dfeb7eec3b729b74827bd63ff1ee4144bcef58b
valnmv <vnaumov@yahoo.com>
2018-12-25 19:14:37 +0200

    Add timeout

--- web-client/main.go
+++ web-client/main.go
@@ -5,9 +5,8 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
-	"net/http/httputil"
-	"os"
 	"strings"
+	"time"
 )
 
 func getExample() {
@@ -44,41 +43,42 @@ func postExample() {
 }
 
 func customClientExample() {
-	debug := os.Getenv("DEBUG")
-	fmt.Println("DEBUG=", debug)
-	client := &http.Client{}
+	//	debug := os.Getenv("DEBUG")
+	client := &http.Client{
+		Timeout: 1000 * time.Millisecond,
+	}
 	request, err := http.NewRequest("GET", "https://ifconfig.co", nil)
 	request.Header.Add("Accept", "application/json")
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	if debug == "1" {
-		debugRequest, err := httputil.DumpRequestOut(request, true)
-		if err != nil {
-			log.Fatal(err)
-		}
+	// if debug == "1" {
+	// 	debugRequest, err := httputil.DumpRequestOut(request, true)
+	// 	if err != nil {
+	// 		log.Fatal(err)
+	// 	}
 
-		fmt.Printf("%s", debugRequest)
-	}
+	// 	fmt.Printf("%s", debugRequest)
+	// }
 
 	response, err := client.Do(request)
 
-	if debug == "1" {
-		debugResponse, err := httputil.DumpResponse(response, true)
-		if err != nil {
-			log.Fatal(err)
-		}
+	// if debug == "1" {
+	// 	debugResponse, err := httputil.DumpResponse(response, true)
+	// 	if err != nil {
+	// 		log.Fatal(err)
+	// 	}
 
-		fmt.Printf("%s", debugResponse)
-	}
+	// 	fmt.Printf("%s", debugResponse)
+	// }
 
-	defer response.Body.Close()
 	body, err := ioutil.ReadAll(response.Body)
 	if err != nil {
 		log.Fatal(err)
 	}
 
+	defer response.Body.Close()
 	fmt.Printf("%s", body)
 }
 

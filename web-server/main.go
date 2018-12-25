b0cf7173007b9caf8e0c2adeced403b54349495c
valnmv <vnaumov@yahoo.com>
2018-12-25 07:09:22 +0200

    Custom header

--- web-server/main.go
+++ web-server/main.go
@@ -3,6 +3,7 @@ package main
 import "net/http"
 
 func hello(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("X-My-Header", "I am setting a header!")
 	w.Write([]byte("Hello World\n"))
 }
 

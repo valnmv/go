d13c5daf7fd20b8e501db284da00ae4b17419aad
valnmv <vnaumov@yahoo.com>
2018-12-26 09:27:10 +0200

    Read dir

--- files/main.go
+++ files/main.go
@@ -15,6 +15,27 @@ func readFile() {
 	fmt.Print(string(bytes))
 }
 
+func writeFile() {
+	s := "Hello World"
+	err := ioutil.WriteFile("example03.txt", []byte(s), 0644)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func readDir() {
+	files, err := ioutil.ReadDir("c:/")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, file := range files {
+		fmt.Println(file.Mode(), file.Name())
+	}
+}
+
 func main() {
 	readFile()
+	writeFile()
+	readDir()
 }

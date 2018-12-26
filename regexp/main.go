b83026ebf7af20afe440e59bf39144071748366c
valnmv <vnaumov@yahoo.com>
2018-12-26 11:35:07 +0200

    Clean HTML tags

--- regexp/main.go
+++ regexp/main.go
@@ -2,11 +2,32 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"log"
+	"net/http"
 	"regexp"
 )
 
-func main() {
+func scrapeHTML() {
+	resp, err := http.Get("https://petition.parliament.uk/petitions")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+	src := string(body)
+	re := regexp.MustCompile("<h3>.*</h3>")
+	titles := re.FindAllString(src, -1)
+	rHTML := regexp.MustCompile("<[^>]*>")
+	for _, title := range titles {
+		fmt.Println(rHTML.ReplaceAllString(title, ""))
+	}
+}
+
+func parse() {
 	needle := "(?i)chocolate"
 	haystack := "Chocolate is my favorite"
 	matched, err := regexp.MatchString(needle, haystack)
@@ -28,4 +49,9 @@ func main() {
 	words2 := "北京市, إسرائيل"
 	fmt.Println(words, re.MatchString(words))
 	fmt.Println(words2, re.MatchString(words2))
+}
+
+func main() {
+	parse()
+	scrapeHTML()
 }

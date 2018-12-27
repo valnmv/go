0dd5897d357cc1d264f7143ea45e779b1fccf409
valnmv <vnaumov@yahoo.com>
2018-12-27 15:02:39 +0200

    Send messages

--- tcp/main.go
+++ tcp/main.go
@@ -1,25 +1,97 @@
 package main
 
 import (
+	"fmt"
+	"io"
 	"log"
 	"net"
 )
 
+// Message struct
+type Message struct {
+	conn    net.Conn
+	message []byte
+}
+
+var connections []net.Conn
+var messages = make(chan Message)
+var addClient = make(chan net.Conn)
+var removeClient = make(chan net.Conn)
+
 func main() {
+
 	server, err := net.Listen("tcp", ":8080")
 	if err != nil {
 		log.Fatal(err)
 	}
-
 	defer server.Close()
+
+	go startChannels()
 
 	for {
 		conn, err := server.Accept()
 		if err != nil {
 			log.Fatal(err)
 		}
-
-		conn.Write([]byte("Hello\n"))
-		conn.Close()
+		addClient <- conn
+		go handleRequest(conn)
 	}
+}
+
+func startChannels() {
+	for {
+		select {
+
+		case msg := <-messages:
+			broadcastMessage(&msg)
+		case newClient := <-addClient:
+			connections = append(connections, newClient)
+			fmt.Println(len(connections))
+		case deadClient := <-removeClient:
+			removeConn(deadClient)
+			fmt.Println(len(connections))
+		}
+	}
+}
+
+func handleRequest(conn net.Conn) {
+	for {
+		m := make([]byte, 1024)
+		_, err := conn.Read(m)
+		if err != nil {
+			if err == io.EOF {
+				removeClient <- conn
+				conn.Close()
+				return
+			}
+			log.Fatal(err)
+		}
+
+		msg := Message{
+			conn:    conn,
+			message: m,
+		}
+		messages <- msg
+	}
+}
+
+func broadcastMessage(m *Message) {
+	for _, conn := range connections {
+		if m.conn != conn {
+			_, err := conn.Write(m.message)
+			if err != nil {
+				log.Fatal(err)
+			}
+		}
+	}
+}
+
+func removeConn(conn net.Conn) {
+	var i int
+	for i = range connections {
+		if connections[i] == conn {
+			break
+		}
+	}
+	connections = append(connections[:i], connections[i+1:]...)
 }

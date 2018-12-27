101de3f5b2a6688ae3199cfc1fa71691e7d91e73
valnmv <vnaumov@yahoo.com>
2018-12-27 10:08:16 +0200

    Add create task test

--- rest-api/main.go
+++ rest-api/main.go
@@ -1,12 +1,15 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
 
 	"github.com/julienschmidt/httprouter"
 )
+
+var store *Store
 
 // Server is wrapper structure for httprouter
 type Server struct {
@@ -20,12 +23,31 @@ func (s *Server) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 
 // ListTasks serves GET requests
 func ListTasks(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
-	fmt.Fprint(w, "ListTasks\n")
+	tasks, err := store.GetTasks()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	b, err := json.Marshal(tasks)
+	w.WriteHeader(http.StatusOK)
+	w.Write(b)
 }
 
 // CreateTask serves POST requests
 func CreateTask(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
-	fmt.Fprint(w, "CreateTasks\n")
+	t := Task{}
+	decoder := json.NewDecoder(r.Body)
+	err := decoder.Decode(&t)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = store.CreateTask(&t)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	w.WriteHeader(http.StatusCreated)
 }
 
 // ReadTask serves GET/:id requests
@@ -43,6 +65,12 @@ func DeleteTask(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 	fmt.Fprint(w, "DeleteTask\n")
 }
 
+// SetStore is used to provide a store initialized from outside.
+// The provider is responsible for closing the store.Used for testing.
+func SetStore(s *Store) {
+	store = s
+}
+
 func main() {
 	router := httprouter.New()
 	router.GET("/", ListTasks)
@@ -50,6 +78,15 @@ func main() {
 	router.GET("/:id", ReadTask)
 	router.PUT("/:id", UpdateTask)
 	router.DELETE("/:id", DeleteTask)
+
+	var err error
+	store, err = NewStore()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	store.Initialize()
+	defer store.Close()
 
 	log.Fatal(http.ListenAndServe(":8080", &Server{router}))
 }
--- rest-api/main_test.go
+++ rest-api/main_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bytes"
 	"net/http"
 	"net/http/httptest"
 	"testing"
@@ -9,6 +10,11 @@ import (
 )
 
 func TestListTasksHandler(t *testing.T) {
+	store, _ := NewStore()
+	store.Initialize()
+	defer store.Close()
+	SetStore(store)
+
 	router := httprouter.New()
 	router.GET("/", ListTasks)
 	r, _ := http.NewRequest("GET", "/", nil)
@@ -28,5 +34,30 @@ func TestListTasksHandler(t *testing.T) {
 		t.Errorf("handler returned unexpected status code: got %v want %v",
 			w.Code, expectedCode)
 	}
+}
 
+func TestCreateTask(t *testing.T) {
+	store, _ := NewStore()
+	store.Initialize()
+	defer store.Close()
+	SetStore(store)
+
+	router := httprouter.New()
+	router.POST("/", CreateTask)
+	var jsonStr = []byte(`{"title":"Buy cheese and bread for breakfast."}`)
+	r, _ := http.NewRequest("POST", "/", bytes.NewBuffer(jsonStr))
+	w := httptest.NewRecorder()
+	s := Server{router}
+	s.ServeHTTP(w, r)
+
+	expectedContentTypeHeader := "application/json; charset=utf-8"
+	if w.Header().Get("Content-Type") != expectedContentTypeHeader {
+		t.Errorf("handler returned unexpected Content-Type header: got %v want %v",
+			w.Header().Get("Content-Type"), expectedContentTypeHeader)
+	}
+
+	expectedCode := http.StatusCreated
+	if w.Code != expectedCode {
+		t.Errorf("handler returned unexpected status code: got %v want %v", w.Code, expectedCode)
+	}
 }
